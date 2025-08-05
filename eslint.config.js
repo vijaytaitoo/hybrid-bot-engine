d8fb7cb45afbd26cec1c632192bf9ca7161aef19
nikola7638 <vacula76@mail.ru>
2025-08-06 03:21:09 +0500

    feat: Add auto-removal rules for console.log, debugger, and TODO comments

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,20 @@ import prettier from 'eslint-plugin-prettier';
 export default [
   js.configs.recommended,
   {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'out/',
+      'build/',
+      '.env',
+      '.env.*',
+      '*.log',
+      '.DS_Store',
+      '.next/',
+      '.vercel/',
+    ],
+  },
+  {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: tsparser,
@@ -29,6 +43,9 @@ export default [
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+      'no-warning-comments': ['warn', { terms: ['todo', 'fixme'], location: 'start' }],
     },
   },
   {
